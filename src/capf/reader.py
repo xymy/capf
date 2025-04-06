7f0506b77cd84a68fe32a467c9d594775737084b
xymy <thyfan@163.com>
2025-04-06 19:30:34 +0800

    Update reader

--- src/capf/reader.py
+++ src/capf/reader.py
@@ -1,37 +1,33 @@
 from collections.abc import Sequence
 
 
-class ReaderError(Exception):
-    """Exception raised by :class:`Reader`."""
-
-
 class Reader:
     """Wrapper for stream reading with rollback.
 
     Parameters
     ----------
-    units : Sequence[str]
-        Sequence of string unit to read.
+    tokens : collections.abc.Sequence[str]
+        The tokens to read.
     """
 
-    def __init__(self, units: Sequence[str]) -> None:
-        self.units = units
-        self.index = 0
+    def __init__(self, tokens: Sequence[str]) -> None:
+        self._tokens = tokens
+        self._cursor = 0
 
     def is_eof(self) -> bool:
-        """Return ``True`` if there is no more unit."""
-        return self.index >= len(self.units)
+        """Return ``True`` if there is no more token."""
+        return self._cursor >= len(self._tokens)
 
     def get(self) -> str:
-        """Get the current unit and advance internal cursor."""
-        if self.index >= len(self.units):
-            raise ReaderError("index out of range")
-        line = self.units[self.index]
-        self.index += 1
-        return line
+        """Get the current token and advance internal cursor."""
+        if self._cursor >= len(self._tokens):
+            raise IndexError("index out of range")
+        token = self._tokens[self._cursor]
+        self._cursor += 1
+        return token
 
     def put(self) -> None:
         """Reverse advance internal cursor."""
-        if self.index == 0:
-            raise ReaderError("index out of range")
-        self.index -= 1
+        if self._cursor == 0:
+            raise IndexError("index out of range")
+        self._cursor -= 1
